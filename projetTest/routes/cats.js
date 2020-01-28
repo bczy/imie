a5aa0275a2f8d02e1e3000974ba4a97a170a1e7b
trambz <bertrand.coizy@gmail.com>
2020-01-28 20:36:00 +0100

    Cleaning, refactoring and using ES6 modules

--- projetTest/routes/cats.js
+++ projetTest/routes/cats.js
@@ -1,22 +1,20 @@
-const express = require('express')
-const router = express.Router()
+import express from 'express';
 
-router.get('/', function (req, res, next) {
-  const cats = [
-    'https://purr.objects-us-east-1.dream.io/i/smPafU3.jpg',
-    'https://purr.objects-us-east-1.dream.io/i/capturadepantalla2017-12-17alas9.34.38p.m..png',
-    'https://purr.objects-us-east-1.dream.io/i/20161027_175945.jpg',
-    'https://purr.objects-us-east-1.dream.io/i/43LBW.jpg',
-    'https://purr.objects-us-east-1.dream.io/i/c5OYV.jpg',
-    'https://purr.objects-us-east-1.dream.io/i/image1a.jpg',
-    'https://purr.objects-us-east-1.dream.io/i/rme9h.jpg',
-    'https://purr.objects-us-east-1.dream.io/i/yPjrX.jpg'
-  ]
+export const catsRouter = express.Router();
 
-  const randomIndex = Math.floor(cats.length * Math.random())
+const cats = [
+  'https://purr.objects-us-east-1.dream.io/i/smPafU3.jpg',
+  'https://purr.objects-us-east-1.dream.io/i/capturadepantalla2017-12-17alas9.34.38p.m..png',
+  'https://purr.objects-us-east-1.dream.io/i/20161027_175945.jpg',
+  'https://purr.objects-us-east-1.dream.io/i/43LBW.jpg',
+  'https://purr.objects-us-east-1.dream.io/i/c5OYV.jpg',
+  'https://purr.objects-us-east-1.dream.io/i/image1a.jpg',
+  'https://purr.objects-us-east-1.dream.io/i/rme9h.jpg',
+  'https://purr.objects-us-east-1.dream.io/i/yPjrX.jpg'
+];
 
-  const urlRandom = cats[randomIndex]
-  res.json({ urlRandom })
-})
-
-module.exports = router
+catsRouter.get('/random', (req, res, next) => {
+  const randomIndex = Math.floor(cats.length * Math.random());
+  const urlRandom = cats[randomIndex];
+  res.json({ urlRandom });
+});
--- projetTest/routes/index.js
+++ projetTest/routes/index.js
@@ -1,9 +1,7 @@
-const express = require('express')
-const router = express.Router()
+import express from 'express';
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
-})
+export const indexRouter = express.Router();
 
-module.exports = router
+indexRouter.get('/', (req, res) => {
+  res.render('index', { title: 'Express' });
+});
