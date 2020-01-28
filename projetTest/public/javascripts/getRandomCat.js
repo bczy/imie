caea2e897bb228d692ba8ddcd0a03e276d361e63
trambz <bertrand.coizy@gmail.com>
2020-01-28 20:36:38 +0100

    renaming getCat client js file

---
+++ projetTest/public/javascripts/getRandomCat.js
@@ -0,0 +1,18 @@
+let catImage = undefined
+window.onload = () => {
+  catImage = document.getElementById('catImage')
+  getRandomCat()
+}
+function getRandomCat () {
+  fetch('/cats/random')
+    .then(response => {
+      return response.json()
+    })
+    .then(json => {
+      // Work with JSON data here
+      if (catImage) {
+        catImage.src = json.urlRandom
+        catImage.style.width = '640px'
+      }
+    })
+}
